3b02377c56771718788016143a3852efe946b327
YuvalYahav <62018841+YuvalYahav@users.noreply.github.com>
2020-03-17 11:30:19 +0200

    spinner

--- app/src/main/java/com/example/planmycday/CreateBehavior.java
+++ app/src/main/java/com/example/planmycday/CreateBehavior.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,12 +19,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CreateBehavior extends AppCompatActivity {
    private Button btninfo, btnplan;
    private Spinner spinner;
 
-    //ValueEventListener listener;
+    ValueEventListener listener;
    // ArrayAdapter<String> adapter;
     //ArrayList<String> spinnerDataList;
     DatabaseReference reference;
@@ -50,15 +53,55 @@ public class CreateBehavior extends AppCompatActivity {
             }
         });
 
-       // spinner=findViewById(R.id.spinner);
-     //   ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.numbers, android)
+        reference= FirebaseDatabase.getInstance().getReference().child("Try");
+
+
+        spinner=(Spinner) findViewById(R.id.spinner);
+        final ArrayList<String> list=new ArrayList<>();
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        listener=reference.addValueEventListener(new ValueEventListener() {
+             @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot item : dataSnapshot.getChildren()) {
+                list.add(item.getValue().toString());
+            }
+            adapter.notifyDataSetChanged();
+        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                String itemvalue=parent.getItemAtPosition(position).toString();
+                Toast.makeText(CreateBehavior.this, "Selected "+itemvalue, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+
+
+
+         // spinner=findViewById(R.id.spinner);
+        //ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.numbers, android)
         //spinnerDataList = new ArrayList<>();
         //reference= FirebaseDatabase.getInstance().getReference().child("Try");
         //adapter=new ArrayAdapter<String>(CreateBehavior.this, android.R.layout.simple_spinner_dropdown_item,spinnerDataList);
 
 
         //retrieveData();
-      //  spinner.setAdapter(adapter);
+        // spinner.setAdapter(adapter);
     }
 
    // public void retrieveData(){
