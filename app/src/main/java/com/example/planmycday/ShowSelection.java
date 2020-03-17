03a6363953b917baaea56d8f981045d9bfd1726b
einav <einavi114@gmail.com>
2020-03-17 11:31:38 +0200

    add show selection

---
+++ app/src/main/java/com/example/planmycday/ShowSelection.java
@@ -0,0 +1,14 @@
+package com.example.planmycday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_selection);
+    }
+}
